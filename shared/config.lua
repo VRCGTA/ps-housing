2ed332215e373bb911ab9c732cb9e2515d6b6a4a
azutake <azusa_takemae@dwango.co.jp>
2024-01-22 01:51:21 +0900

    fix stash weight

--- shared/config.lua
+++ shared/config.lua
@@ -172,7 +172,7 @@ Config.Shells = {
         hash = `furnitured_midapart`,
         doorOffset = { x = 1.44, y = -10.25, z = 0.0, h = 0.0, width = 1.5  },
         stash = {
-            maxweight = 1000000, 
+            maxweight = 5000000, 
             slots = 50,
         },
         imgs = {
